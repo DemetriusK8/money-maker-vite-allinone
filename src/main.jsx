145179bb565e4baf52fadc66e493d50cffcf8c0e
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-05 17:01:06 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,20 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+// Pinterest Tag
+!function(e){if(!window.pintrk){window.pintrk = function () {
+window.pintrk.queue.push(Array.prototype.slice.call(arguments))}};
+var
+n=window.pintrk;n.queue=[],n.version="3.0";
+var t=document.createElement("script");
+t.async=!0;t.src=e;
+var r=document.getElementsByTagName("script")[0];
+r.parentNode.insertBefore(t,r)}
+("https://s.pinimg.com/ct/core.js");
+
+pintrk('load','2612412658221');
+pintrk('page');
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -11,3 +25,5 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 )
+
+
