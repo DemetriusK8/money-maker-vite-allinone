be0b6a914fc708a35f669df0133c5c6bda78879c
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-05 17:35:56 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,37 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-// Pinterest Tag
-!function(e){if(!window.pintrk){window.pintrk = function () {
-window.pintrk.queue.push(Array.prototype.slice.call(arguments))}};
-var
-n=window.pintrk;n.queue=[],n.version="3.0";
-var t=document.createElement("script");
-t.async=!0;t.src=e;
-var r=document.getElementsByTagName("script")[0];
-r.parentNode.insertBefore(t,r)}
-("https://s.pinimg.com/ct/core.js");
+// --- Pinterest Tag setup ---
+function initPinterestTag() {
+  // Make sure we're in the browser, not during any server-side build
+  if (typeof window === 'undefined') return;
 
-pintrk('load','2612412658221');
-pintrk('page');
+  (function (e) {
+    if (!window.pintrk) {
+      window.pintrk = function () {
+        window.pintrk.queue.push(Array.prototype.slice.call(arguments))
+      }
+      const n = window.pintrk
+      n.queue = []
+      n.version = '3.0'
 
+      const t = document.createElement('script')
+      t.async = true
+      t.src = e
+
+      const r = document.getElementsByTagName('script')[0]
+      r.parentNode.insertBefore(t, r)
+    }
+  })('https://s.pinimg.com/ct/core.js')
+
+  window.pintrk('load', '2612412658221')
+  window.pintrk('page')
+}
+
+// Run the Pinterest tag once when the app starts
+initPinterestTag()
+
+// --- React app mount ---
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
