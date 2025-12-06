1eede8860e1c790f27a0ef08ce977b7ce56680bc
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-05 18:29:26 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,37 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-// --- Pinterest Tag setup ---
-function initPinterestTag() {
-  // Make sure we're in the browser, not during any server-side build
-  if (typeof window === 'undefined') return;
-
-  (function (e) {
-    if (!window.pintrk) {
-      window.pintrk = function () {
-        window.pintrk.queue.push(Array.prototype.slice.call(arguments))
-      }
-      const n = window.pintrk
-      n.queue = []
-      n.version = '3.0'
-
-      const t = document.createElement('script')
-      t.async = true
-      t.src = e
-
-      const r = document.getElementsByTagName('script')[0]
-      r.parentNode.insertBefore(t, r)
-    }
-  })('https://s.pinimg.com/ct/core.js')
-
-  window.pintrk('load', '2612412658221')
-  window.pintrk('page')
-}
-
-// Run the Pinterest tag once when the app starts
-initPinterestTag()
-
-// --- React app mount ---
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -42,5 +11,3 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>
 )
-
-
