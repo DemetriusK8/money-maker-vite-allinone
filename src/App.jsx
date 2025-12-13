d913b60098851050ead7abde64806aa3825a9158
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-13 12:39:32 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
-<Routes>
-  <Route path="*" element={<div>ROUTER WORKS</div>} />
-</Routes>
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Deals from "./pages/Deals";
+import Trending from "./pages/Trending";
+import Hustles from "./pages/Hustles";
+import AutoDeals from "./pages/AutoDeals";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/deals" element={<Deals />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/hustles" element={<Hustles />} />
+        <Route path="/auto-deals" element={<AutoDeals />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
