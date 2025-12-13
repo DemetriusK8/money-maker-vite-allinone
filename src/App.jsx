5b20fcf6a5252c04d53d1f24973ff11714b25528
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-13 11:17:20 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Deals from "./pages/Deals";
 
 function Home() {
   return <h1 style={{ padding: 40 }}>Home is working</h1>;
@@ -9,6 +10,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/deals" element={<Deals />} />
       </Routes>
     </BrowserRouter>
   );
