a83ade43d1c873cd25965e912b384b9d7abca84e
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-13 08:48:37 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Deals from "./pages/Deals";
-import Trending from "./pages/Trending";
-import Hustles from "./pages/Hustles";
-import AutoDeals from "./pages/AutoDeals";
-import BottomNav from "./components/BottomNav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/deals" element={<Deals />} />
-        <Route path="/trending" element={<Trending />} />
-        <Route path="/hustles" element={<Hustles />} />
-        <Route path="/auto-deals" element={<AutoDeals />} />
-      </Routes>
-
-      <BottomNav />
-    </>
-  );
+function Home() {
+  return <h1 style={{ padding: 40 }}>Home is working</h1>;
 }
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
