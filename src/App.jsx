2757a58e47f53e49da200093608df1d0144cdb86
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-13 13:46:35 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Deals from "./pages/Deals";
-import Trending from "./pages/Trending";
-import Hustles from "./pages/Hustles";
-import AutoDeals from "./pages/AutoDeals";
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/deals" element={<Deals />} />
-        <Route path="/trending" element={<Trending />} />
-        <Route path="/hustles" element={<Hustles />} />
-        <Route path="/auto-deals" element={<AutoDeals />} />
-      </Routes>
-    </BrowserRouter>
+    <div style={{ padding: 40 }}>
+      <h1>🔥 APP IS RENDERING</h1>
+      <p>If you see this, React + Vite work.</p>
+    </div>
   );
 }
