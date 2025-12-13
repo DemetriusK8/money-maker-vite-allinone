795a2c303bdad712df3fa7986493e445a8c6ab18
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-13 12:00:48 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Deals from "./pages/Deals";
-
-function Home() {
-  return <h1 style={{ padding: 40 }}>Home is working</h1>;
-}
+import Trending from "./pages/Trending";
+import Hustles from "./pages/Hustles";
+import AutoDeals from "./pages/AutoDeals";
 
 export default function App() {
   return (
@@ -11,6 +11,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/deals" element={<Deals />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/hustles" element={<Hustles />} />
+        <Route path="/auto-deals" element={<AutoDeals />} />
       </Routes>
     </BrowserRouter>
   );
