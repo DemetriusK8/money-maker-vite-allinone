52639bc5498859bfea7a36efd51f049273604fea
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-15 12:57:41 -0600

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,3 @@
-// src/pages/Home.jsx
-import { Link } from 'react-router-dom';
-import { mainOffers } from "../data/allDeals";
 export default function Home() {
-  return (
-    <div className="flex flex-col">
-      
-      {/* HERO SECTION */}
-      <section className="text-center py-24 bg-gradient-to-br from-gray-900 via-black to-[#C8A951] text-white shadow-lg">
-        <h1 className="text-5xl font-extrabold mb-6 drop-shadow-md">
-          Make Money Online. Faster. 🚀
-        </h1>
-
-        <p className="text-lg max-w-2xl mx-auto opacity-90 mb-10 font-light">
-          The smartest
+  return <h1>🏠 Home Page</h1>;
+}
