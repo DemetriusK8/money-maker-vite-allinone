adf9bfb472f4f0c51013b5b8853782c4f61b5be4
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-16 16:22:42 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Deals from "./pages/Deals";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-function BottomNav() {
-  return (
-    <nav
-      style={{
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        display: "flex",
-        justifyContent: "space-around",
-        padding: "12px 0",
-        background: "#111",
-        color: "#fff",
-      }}
-    >
-      <Link style={{ color: "#fff" }} to="/">Home</Link>
-      <Link style={{ color: "#fff" }} to="/deals">Deals</Link>
-    </nav>
-  );
-}
-
-export default function App() {
-  return (
-    <HashRouter>
-      <div style={{ paddingBottom: 70 }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/deals" element={<Deals />} />
-        </Routes>
-      </div>
-      <BottomNav />
-    </HashRouter>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
