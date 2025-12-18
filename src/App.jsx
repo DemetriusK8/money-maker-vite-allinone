af4d9e017203a86dcad8f26ea25d1d7c45f81027
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-18 11:12:18 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Deals from "./pages/Deals";
 
 export default function App() {
   return (
-    <div>
-      <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
-        <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
-        <Link to="/deals">Deals</Link>
-      </nav>
-
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/deals" element={<Deals />} />
       </Routes>
-    </div>
+    </HashRouter>
   );
 }
