6f2847b1984c75f4c311d38ae6e2bc6ad817c4c8
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-18 18:13:10 -0600

    Update Hustles.jsx

--- src/pages/Hustles.jsx
+++ src/pages/Hustles.jsx
@@ -1,13 +1,3 @@
-import OfferGrid from "../components/OfferGrid";
-import { hustleIdeas } from "../data/allDeals";
-
 export default function Hustles() {
-  return (
-    <main className="max-w-6xl mx-auto px-4 py-12">
-      <OfferGrid
-        title="💡 Side Hustle Ideas"
-        items={hustleIdeas}
-      />
-    </main>
-  );
+  return <h1>💼 Side Hustle Ideas (Coming Soon)</h1>;
 }
