80b1a24c229fa530c2f6440a03f1dd08025354f2
DemetriusK8 <demetriuskeyes@yahoo.com>
2025-12-18 18:09:10 -0600

    Update Trending.jsx

--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -1,13 +1,3 @@
-import OfferGrid from "../components/OfferGrid";
-import { trendingOffers } from "../data/allDeals";
-
 export default function Trending() {
-  return (
-    <main className="max-w-6xl mx-auto px-4 py-12">
-      <OfferGrid
-        title="🔥 Trending Offers"
-        items={trendingOffers}
-      />
-    </main>
-  );
+  return <h1>🔥 Trending Offers (Coming Soon)</h1>;
 }
