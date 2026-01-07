899014fa0868b06927961bf4742160b297a4c6aa
DemetriusK8 <demetriuskeyes@yahoo.com>
2026-01-06 22:39:39 -0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import LeadMagnet from '../components/LeadMagnet';
 import { mainOffers } from "../data/allDeals";
-
+import MailerLiteForm from "../components/MailerLiteForm";
 export default function Home() {
   const topOffer = mainOffers[0]; // Ikaria
 
