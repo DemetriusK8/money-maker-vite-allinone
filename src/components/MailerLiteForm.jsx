8d9df37c640c0e824f996ad91cf25b91bf39800d
DemetriusK8 <demetriuskeyes@yahoo.com>
2026-01-12 14:09:58 -0600

    Update MailerLiteForm.jsx

--- src/components/MailerLiteForm.jsx
+++ src/components/MailerLiteForm.jsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
+
 export default function MailerLiteForm() {
+  useEffect(() => {
+    // Load MailerLite script only once
+    if (document.getElementById("mailerlite-universal")) return;
+
+    const script = document.createElement("script");
+    script.id = "mailerlite-universal";
+    script.src = "https://assets.mailerlite.com/js/universal.js";
+    script.async = true;
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <div style={{ marginTop: "2rem" }}>
-      <div className="ml-embedded" data-form="NtFURj"></div>
+      <div
+        className="ml-embedded"
+        data-form="NtFURj"
+      ></div>
     </div>
   );
 }
