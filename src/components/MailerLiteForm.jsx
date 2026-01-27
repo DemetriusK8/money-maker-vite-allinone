5124535e5f239e23bc6ab807b42aca4559c31718
DemetriusK8 <demetriuskeyes@yahoo.com>
2026-01-27 15:23:38 -0600

    Update MailerLiteForm.jsx

--- src/components/MailerLiteForm.jsx
+++ src/components/MailerLiteForm.jsx
@@ -1,17 +1,25 @@
-return (
-  <div
-    style={{
-      marginTop: "2rem",
-      position: "relative",
-      zIndex: 9999,
-      pointerEvents: "auto"
-    }}
-  >
-    <div
-      id="ml-form-placeholder"
-      className="ml-embedded"
-      data-form="NtFU9"
-      style={{ pointerEvents: "auto" }}
-    ></div>
-  </div>
-);
+import { useEffect } from "react";
+
+export default function MailerLiteForm() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://assets.mailerlite.com/js/universal.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      if (window.ml) {
+        window.ml("account", "1993251");
+      }
+    };
+  }, []);
+
+  return (
+    <div style={{ marginTop: "30px", position: "relative", zIndex: 999 }}>
+      <div
+        className="ml-embedded"
+        data-form="NtFU9"
+      ></div>
+    </div>
+  );
+}
